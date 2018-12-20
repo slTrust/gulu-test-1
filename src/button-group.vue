32e9f3948b722c57306882a90dd19a3f5e2ab16f
huangjiaxi <trustfor@sina.cn>
2018-12-20 22:24:32 +0800

    /*  如果不是第一个元素，左边框就没  此时导致了一个bug 就是 hover的时候 会导致只剩下三个方向的边框  */
     &:not(:first-child){border-left:none;}
    
     所以删掉 另想一个办法

--- src/button-group.vue
+++ src/button-group.vue
@@ -14,6 +14,13 @@
         vertical-align: middle;
         > .g-button{
             border-radius: 0;
+            /*
+             如果不是第一个元素，左边框就没
+             此时导致了一个bug 就是 hover的时候 会导致只剩下三个方向的边框
+            */
+
+            &:not(:first-child){border-left:none;}
+
             &:first-child{
                 border-top-left-radius: var(--border-radius);
                 border-bottom-left-radius: var(--border-radius);
