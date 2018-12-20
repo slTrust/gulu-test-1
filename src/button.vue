a40996187eb14806b4e1b0f9e3bb38bd65513c14
huangjiaxi <trustfor@sina.cn>
2018-12-20 21:10:58 +0800

    解决图标按钮左右问题： -避免使用v-if 和v-else的重复(重复就是bug) 1. 使用 :class="{`icon-${iconPosition}`:true}" 2. 通过 inline-flex 的 order来控制 内容的先后 3. 注意注意  slot是不能添加class 所以外层嵌套div来解决

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,18 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
-    export default {}
+    export default {
+        props:['icon','iconPosition']
+    }
 </script>
 
 <style scoped lang="scss">
@@ -14,14 +23,18 @@
         border-radius: var(--border-radius);
         border:1px solid var(--border-color);
         background: var(--button-bg);
-        &:hover{
-            border-color: var(--border-color-hover);
-        }
-        &:active{
-            background-color: var(--button-active-bg);
-        }
-        &:focus{
-            outline: none;
+        justify-content: center;
+        align-items: center;
+        display:inline-flex;
+        &:hover{border-color: var(--border-color-hover);}
+        &:active{background-color: var(--button-active-bg);}
+        &:focus{outline: none;}
+
+        > .icon{order:1;}
+        > .content{order:2;}
+        &.icon-right{
+            > .icon{order:2;}
+            > .content{order:1;}
         }
     }
 
