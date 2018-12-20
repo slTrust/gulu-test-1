3e6bc01eab9df60e0c232e1f57422ea635db0cca
huangjiaxi <trustfor@sina.cn>
2018-12-20 22:17:31 +0800

    添加 button-group 组件 如果包含多个button 中间圆角的处理

---
+++ src/button-group.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped lang="scss">
+    .g-button-group{
+        display:inline-flex;
+        vertical-align: middle;
+        > .g-button{
+            border-radius: 0;
+            &:first-child{
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+        }
+    }
+</style>
