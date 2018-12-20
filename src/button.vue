e51eb8c3e5468dfd4d9d87cd43a86fa330e6cf21
huangjiaxi <trustfor@sina.cn>
2018-12-20 21:51:51 +0800

    添加 loading 状态 keyframes 动画使用

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
         <g-icon v-if="icon" class="icon" :name="icon"></g-icon>
+        <g-icon class="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -24,6 +25,10 @@
 </script>
 
 <style scoped lang="scss">
+    @keyframes spin {
+        0%{transform: rotate(0deg);}
+        100%{transform: rotate(360deg);}
+    }
     .g-button{
         font-size: var(--font-size);
         height: var(--button-height);
@@ -44,6 +49,10 @@
         &.icon-right{
             > .icon{order:2; margin-left:.1em;margin-right:0;}
             > .content{order:1;}
+        }
+
+        .loading{
+            animation: spin 2s infinite linear;
         }
     }
 
