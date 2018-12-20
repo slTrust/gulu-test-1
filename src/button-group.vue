37efcfd21d0ca2c5577cfa9435008c30300b6fab
huangjiaxi <trustfor@sina.cn>
2018-12-20 22:30:15 +0800

    创建button-group 基本实现了需求； 使用 margin负值 margin-left:-1px; 在hover的瞬间 z-index增加权重
    同时不要忘了 重置第一个元素的margin-left:0;

--- src/button-group.vue
+++ src/button-group.vue
@@ -17,17 +17,25 @@
             /*
              如果不是第一个元素，左边框就没
              此时导致了一个bug 就是 hover的时候 会导致只剩下三个方向的边框
+             &:not(:first-child){border-left:none;}
+             使用 margin负值
+             在hover的瞬间 z-index增加权重
             */
-
-            &:not(:first-child){border-left:none;}
+            margin-left:-1px;
 
             &:first-child{
+                margin-left:0px; /* 移除第一个元素的margin负值 */
                 border-top-left-radius: var(--border-radius);
                 border-bottom-left-radius: var(--border-radius);
             }
             &:last-child{
                 border-top-right-radius: var(--border-radius);
                 border-bottom-right-radius: var(--border-radius);
+            }
+
+            &:hover{
+                position: relative;
+                z-index: 1;
             }
         }
     }
