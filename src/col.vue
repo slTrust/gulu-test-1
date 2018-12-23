5778d2517e07aa0e652b9b0f910be733b6ce4647
huangjiaxi <trustfor@sina.cn>
2018-12-23 17:06:58 +0800

    初步实现 响应式

--- src/col.vue
+++ src/col.vue
@@ -5,6 +5,16 @@
 </template>
 
 <script>
+    let validator = (value)=>{
+        let keys = Object.keys(value);
+        let valid = true;
+        keys.forEach((key)=>{
+            if( !['span','offset'].includes(key)){
+                valid = false;
+            }
+        })
+        return valid;
+    }
     export default {
         name: "GuluCol",
         props:{
@@ -14,6 +24,11 @@
             offset:{
                 type:[Number,String]
             },
+            phone:{type:Object,validator},
+            ipad:{type:Object, validator},
+            narrowPc:{type:Object, validator},
+            pc:{type:Object, validator},
+            widePc:{type:Object, validator}
         },
         data(){
            return {
@@ -22,10 +37,17 @@
         },
         computed:{
             colClass(){
-                let {span,offset} = this;
+                let {span,offset,phone,ipad,narrowPc,pc,widePc} = this;
+                let phoneClass = [];
+
                 return [
                         span && `col-${span}`,
-                        offset && `offset-${offset}`
+                        offset && `offset-${offset}`,
+                        ...(phone && [`col-phone-${phone.span}`]),
+                        ...(ipad && [`col-ipad-${ipad.span}`]),
+                        ...(narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
+                        ...(pc && [`col-pc-${pc.span}`]),
+                        ...(widePc && [`col-wide-pc-${widePc.span}`])
                 ];
             },
             colStyle(){
@@ -58,6 +80,94 @@
 
         &.#{$class-prefix}#{$n} {
             margin-left:($n/24) * 100%;
+        }
+    }
+    /*手机上最大宽度是 576 */
+    @media (max-width:576px) {
+        $class-prefix: col-phone-;
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                width:($n/24) * 100%;
+            }
+        }
+        $class-prefix: offset-phone-;
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                margin-left:($n/24) * 100%;
+            }
+        }
+    }
+    /*ipad 上最大宽度是 768 最小 577 */
+    @media (min-width:577px) and (max-width:768px) {
+        $class-prefix: col-ipad-;
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                width:($n/24) * 100%;
+            }
+        }
+        $class-prefix: offset-ipad-;
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                margin-left:($n/24) * 100%;
+            }
+        }
+    }
+
+    /* 窄屏幕 上最大宽度是 992 最小 769 */
+    @media (min-width:769px) and (max-width:992px) {
+        $class-prefix: col-narrow-pc-;
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                width:($n/24) * 100%;
+            }
+        }
+        $class-prefix: offset-narrow-pc-;
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                margin-left:($n/24) * 100%;
+            }
+        }
+    }
+
+    /*  上最大宽度是 1200 最小 993 */
+    @media (min-width:993px) and (max-width:1200px) {
+        $class-prefix: col-pc-;
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                width:($n/24) * 100%;
+            }
+        }
+        $class-prefix: offset-pc-;
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                margin-left:($n/24) * 100%;
+            }
+        }
+    }
+
+    /* 超大屏 上最小宽度是 1201 */
+    @media (min-width:1201px) {
+        $class-prefix: col-wide-pc-;
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                width:($n/24) * 100%;
+            }
+        }
+        $class-prefix: offset-wide-pc-;
+        @for $n from 1 through 24 {
+
+            &.#{$class-prefix}#{$n} {
+                margin-left:($n/24) * 100%;
+            }
         }
     }
 </style>
