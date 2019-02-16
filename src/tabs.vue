8a1a7aaf08bb10a0b6fff7b8cbaf8fcfb77d8072
huangjiaxi <trustfor@sina.cn>
2019-02-16 22:50:51 +0800

    重构 tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -34,23 +34,30 @@
         created(){
         },
         mounted(){
-            // $children 只能获取到子组件 不能获取子元素
-            if(this.$children.length === 0 ){
-                console && console.warn &&
-                console.warn('tabs子组件应该是 tabs-head和tabs-body,但是你没有写子组件')
-                throw new Error('tabs子组件应该是 tabs-head和tabs-body,但是你没有写子组件')
-            }
-
-            // emit 多传递一个 this，但是tabs里没有 只能遍历子元素
-            this.$children.forEach((vm)=>{
-                if(vm.$options.name === 'GuluTabsHead'){
-                    vm.$children.forEach((childVm)=>{
-                        if(childVm.$options.name==='GuluTabsItem' && childVm.name === this.selected ){
-                            this.eventBus.$emit('update:selected',this.selected,childVm);
-                        }
-                    })
+            this.checkChild();
+            this.selectTab();
+        },
+        methods:{
+            checkChild(){
+                // $children 只能获取到子组件 不能获取子元素
+                if(this.$children.length === 0 ){
+                    console && console.warn &&
+                    console.warn('tabs子组件应该是 tabs-head和tabs-body,但是你没有写子组件')
+                    throw new Error('tabs子组件应该是 tabs-head和tabs-body,但是你没有写子组件')
                 }
-            })
+            },
+            selectTab(){
+                // emit 多传递一个 this，但是tabs里没有 只能遍历子元素
+                this.$children.forEach((vm)=>{
+                    if(vm.$options.name === 'GuluTabsHead'){
+                        vm.$children.forEach((childVm)=>{
+                            if(childVm.$options.name==='GuluTabsItem' && childVm.name === this.selected ){
+                                this.eventBus.$emit('update:selected',this.selected,childVm);
+                            }
+                        })
+                    }
+                })
+            }
         }
     }
 </script>
