7802bbc55d84a1ed621538460a1fb158d290d44c
huangjiaxi <trustfor@sina.cn>
2019-03-01 21:46:04 +0800

    新增两个单元测试 button 和 popover

---
+++ tests/unit/popover.spec.js
@@ -0,0 +1,48 @@
+import chai,{ expect } from 'chai'
+import { shallowMount,mount } from '@vue/test-utils'
+import Vue from 'vue'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import Popover from '@/popover'
+chai.use(sinonChai)
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Popover', () => {
+    it('存在.', () => {
+        expect(Popover).to.be.exist
+    })
+    it('可以设置position', () => {
+        const wrapper = mount(Popover,{
+            slots:{
+                default:{template:`<button>点我</button>`},
+                content:'弹出内容'
+            },
+            propsData:{
+                position:'bottom'
+            }
+        })
+        wrapper.find('button').trigger('click');
+        let classes = wrapper.find('.content-wrapper').classes()
+        expect(classes).to.include('position-bottom')
+    })
+
+    /* 遗留问题 无法触发hover */
+    it('可以设置 trigger', () => {
+        const wrapper = mount(Popover,{
+            slots:{
+                default:{template:`<button>点我</button>`},
+                content:'弹出内容'
+            },
+            propsData:{
+                position:'bottom',
+                trigger:'hover'
+            }
+        })
+        expect(wrapper.find('.content-wrapper').element).to.not.exist
+        wrapper.find('.popover').trigger('mouseenter');
+        expect(wrapper.find('.content-wrapper').element).to.exist
+    })
+
+})
