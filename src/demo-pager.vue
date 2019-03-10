dcabc622bf7deb14075dba5330bba282be26d961
huangjiaxi <trustfor@sina.cn>
2019-03-10 13:38:24 +0800

    021 实现pager 基本结构和样式

---
+++ src/demo-pager.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <g-pager :total-page="20" :current-page="3"></g-pager>
+    </div>
+</template>
+
+<script>
+    import GPager from './pager';
+
+    export default {
+        components:{
+            GPager
+        },
+        data(){
+            return {
+            }
+        },
+        created(){
+
+        },
+        methods:{
+            onChange(selected){
+                if(selected ==='home'){
+                    alert('hi')
+                }
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    *{margin:0;padding:0;box-sizing:border-box;}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 // import App from './demo2-slides.vue'
-import App from './demo-nav.vue'
+// import App from './demo-nav.vue'
+import App from './demo-pager.vue'
 
 Vue.config.productionTip = false
 
