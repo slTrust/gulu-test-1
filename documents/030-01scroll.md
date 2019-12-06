8bd1957571a5cc774b336c14429aa83563b2b0d5
huangjiaxi <huangjiaxi@oak-inc.com>
2019-12-06 11:50:14 +0800

    创建 scroll 组件

--- documents/030-01scroll.md
+++ documents/030-01scroll.md
@@ -287,3 +287,65 @@ mounted(){
     })
 },
 ```
+
+> 优化 滚动问题已经滚到边界的时候允许整个页面被拖动
+
+```
+<template>
+    <div class="gulu-scroll-wrapper" ref="parent">
+        <div ref="child" class="gulu-scroll">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'GuluScroll',
+        mounted(){
+            let parent = this.$refs.parent;
+            let child = this.$refs.child;
+            
+            let translateY = 0;
+            let {height:childHeight} = child.getBoundingClientRect();
+            let {height:parentHeight} = parent.getBoundingClientRect();
+            let {borderTopWidth,borderBottomWidth,paddingTop,paddingBottom} = window.getComputedStyle(parent)
+            borderTopWidth = parseInt(borderTopWidth);
+            borderBottomWidth = parseInt(borderBottomWidth);
+            paddingTop = parseInt(paddingTop);
+            paddingBottom = parseInt(paddingBottom);
+            let maxHeight = childHeight - parentHeight + (borderTopWidth + borderBottomWidth + paddingTop + paddingBottom);
+            // 注意这里 mac 和 win 是反的，我这里只考虑mac了
+            parent.addEventListener('wheel',(e)=>{
+                // 限速
+                if(e.deltaY > 20){
+                    translateY -= 20 * 3    
+                }else if(e.deltaY < -20){
+                    translateY -= -20 * 3    
+                }else{
+                    translateY -= e.deltaY * 3
+                }
+                if(translateY > 0){
+                    translateY = 0;
+                }else if(translateY < -maxHeight){
+                    translateY = -maxHeight
+                }else{
+                    // 只有不在边界的时候 组织
+                    e.preventDefault();
+                }
+                child.style.transform = `translateY(${translateY}px)`;
+            })
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+.gulu-scroll{
+    transition: transform 0.05s ease;
+    &-wrapper {
+      overflow: hidden;
+      position: relative;
+    }
+}
+</style>
+```
--- src/demo-scroll.vue
+++ src/demo-scroll.vue
@@ -1,8 +1,8 @@
 <template>
     <div style="margin:20px;">
         
-        <div class="parent" ref="parent">
-            <div class="child" ref="child">
+        <g-scroll class="parent">
+            <div class="child">
                 <p>1</p>
                 <p>2</p>
                 <p>3</p>
@@ -104,7 +104,7 @@
                 <p>99</p>
                 <p>100</p>
             </div>
-        </div>
+        </g-scroll>
     </div>
 </template>
 
@@ -121,37 +121,6 @@
             }
         },
         mounted(){
-            let parent = this.$refs.parent;
-            let child = this.$refs.child;
-            
-            let translateY = 0;
-            child.style.transition = `transform 0.1s ease`;
-            let {height:childHeight} = child.getBoundingClientRect();
-            let {height:parentHeight} = parent.getBoundingClientRect();
-            let {borderTopWidth,borderBottomWidth,paddingTop,paddingBottom} = window.getComputedStyle(parent)
-            borderTopWidth = parseInt(borderTopWidth);
-            borderBottomWidth = parseInt(borderBottomWidth);
-            paddingTop = parseInt(paddingTop);
-            paddingBottom = parseInt(paddingBottom);
-            let maxHeight = childHeight - parentHeight + (borderTopWidth + borderBottomWidth + paddingTop + paddingBottom);
-            // 注意这里 mac 和 win 是反的，我这里只考虑mac了
-            parent.addEventListener('wheel',(e)=>{
-                // 限速
-                if(e.deltaY > 20){
-                    translateY -= 20 * 3    
-                }else if(e.deltaY < -20){
-                    translateY -= -20 * 3    
-                }else{
-                    translateY -= e.deltaY * 3
-                }
-                if(translateY > 0){
-                    translateY = 0;
-                }else if(translateY < -maxHeight){
-                    translateY = -maxHeight
-                }
-                child.style.transform = `translateY(${translateY}px)`;
-
-            })
         },
         methods:{
             
@@ -163,9 +132,8 @@
     *{margin:0;padding:0;box-sizing:border-box;}
     .parent{
         height: 400px;
-        overflow: hidden;
         width:480px;
-        border:10px solid red;
+        border:5px solid red;
         padding:10px;
     }
 
